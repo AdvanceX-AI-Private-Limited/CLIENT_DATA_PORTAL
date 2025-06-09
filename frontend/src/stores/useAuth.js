61d0a3a6fe780590e963c18908fc044fb3b0e844
Aslam Miya <aslam.miya@advancex.ai>
2025-06-09 11:49:59 +0530

    .

--- frontend/src/stores/useAuth.js
+++ frontend/src/stores/useAuth.js
@@ -1,4 +1,15 @@
 <script>
 import { ref } from 'vue';
-const user = ref(null)
+const user = ref(null);
+const loaded = ref(false);
+
+export function useAuthStore() {
+    const isSignedIn = ref(false);
+    
+    return {
+        user,
+        isSignedIn,
+        loaded
+    }
+}
 </script>
