d3ce0c953bdbf6c1599e471029d7161999eb8fbb
Aslam Miya <aslam.miya@advancex.ai>
2025-06-13 15:02:41 +0530

    .

--- frontend/src/views/auth/SignUpView.vue
+++ frontend/src/views/auth/SignUpView.vue
@@ -333,7 +333,7 @@ async function createClient() {
   };
 
   try {
-    // console.log("Payload being sent:", payload);
+    console.log("Payload being sent:", payload);
     const response = await clientsRegister(payload);
     clientApiData.value = response.data;
     
